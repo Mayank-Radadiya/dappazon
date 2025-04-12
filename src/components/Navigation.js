8806ab429db58710707c401fd2a596c45b706932
Mayank-Radadiya <mayankradadiya2@gmail.com>
2025-04-12 21:44:11 +0530

    ADD: Navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,116 @@
-import { ethers } from 'ethers';
+import { ethers } from "ethers";
+import React, { useState, useEffect } from "react";
 
 const Navigation = ({ account, setAccount }) => {
+  const [scrolled, setScrolled] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    return (
-        <nav>
+  const handleWallet = async () => {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    const account = accounts[0];
+    setAccount(account);
+  };
 
-        </nav>
-    );
-}
+  useEffect(() => {
+    handleWallet();
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const accounts = await ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setAccount(accounts[0]);
+      } else {
+        alert(
+          "MetaMask is not installed! Please install it to use this app: https://metamask.io/download.html"
+        );
+      }
+    } catch (error) {
+      console.error("Error connecting to MetaMask:", error);
+    }
+  };
+
+  return (
+    <nav className={scrolled ? "navbar scrolled" : "navbar"}>
+      <div className="navbar-container">
+        <div className="navbar-logo">
+          <span className="logo-text">Dappazon</span>
+          <span className="logo-dot"></span>
+        </div>
+
+        <div
+          className={menuOpen ? "navbar-hamburger active" : "navbar-hamburger"}
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        <ul className={menuOpen ? "navbar-menu active" : "navbar-menu"}>
+          <li className="navbar-item">
+            <a href="#">Home</a>
+          </li>
+          <li className="navbar-item">
+            <a href="#">Shop</a>
+          </li>
+          <li className="navbar-item">
+            <a href="#">Categories</a>
+          </li>
+          <li className="navbar-item">
+            <a href="#">Cart</a>
+          </li>
+          <li className="navbar-item">
+            <a href="#">Profile</a>
+          </li>
+        </ul>
+
+        <div className="navbar-account">
+          {account ? (
+            <div className="account-connected">
+              <div className="account-icon">
+                <span></span>
+              </div>
+              <span className="account-info">
+                {account.slice(0, 6)}...{account.slice(-4)}
+              </span>
+            </div>
+          ) : (
+            <button className="connect-button" onClick={connectWallet}>
+              <span>Connect Wallet</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+                width="16"
+                height="16"
+                fill="currentColor"
+              >
+                <path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z" />
+              </svg>
+            </button>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navigation;
