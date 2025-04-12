e1567397a65f09a1334e8a92b09f1a4eaaeee1b5
Mayank-Radadiya <mayankradadiya2@gmail.com>
2025-04-12 13:01:08 +0530

    ADD: Solidity Code for Backend with all test

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -1,4 +1,101 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-contract Dappazon {}
+contract Dappazon {
+    address public owner;
+
+    // define Item object type.
+    struct Item {
+        uint256 id;
+        string name;
+        string category;
+        string image;
+        uint256 price;
+        uint256 stock;
+        uint256 rating;
+    }
+
+    // Crete a struct of Order
+
+    struct Order {
+        uint256 time;
+        Item item;
+    }
+
+    // map each item with Id
+    mapping(uint256 => Item) public items;
+    mapping(address => uint256) public orderCount;
+    mapping(address => mapping(uint256 => Order)) public orders;
+
+    event ItemCreated(
+        uint256 id,
+        string name,
+        string category,
+        uint256 price,
+        uint256 stock
+    );
+    event BuyItem(address buyer, uint256 orderId, uint256 itemId);
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier OnlyOwner() {
+        // first check if the caller is the owner
+        require(msg.sender == owner, "Only owner can list items");
+        _;
+    }
+
+    function list(
+        uint256 _id,
+        string memory _name,
+        string memory _category,
+        string memory _image,
+        uint256 _price,
+        uint256 _stock,
+        uint256 _rating
+    ) public OnlyOwner {
+        // Crete a item object
+        Item memory newItem = Item(
+            _id,
+            _name,
+            _category,
+            _image,
+            _price,
+            _stock,
+            _rating
+        );
+
+        // store newItem Obj into blockchain
+        items[_id] = newItem;
+
+        // Emit event
+        emit ItemCreated(_id, _name, _category, _price, _stock);
+    }
+
+    function buy(uint256 _id) public payable {
+        // first fetch the item
+        Item memory item = items[_id];
+
+        // send enough ETH to buy item
+        require(msg.value >= items[_id].price, "Not enough ether sent");
+
+        // Crete a order object
+        Order memory order = Order(block.timestamp, item);
+
+        orderCount[msg.sender]++;
+
+        orders[msg.sender][orderCount[msg.sender]] = order;
+
+        require(items[_id].stock > 0, "Out of Stock");
+        items[_id].stock--;
+
+        emit BuyItem(msg.sender, orderCount[msg.sender], items[_id].id);
+    }
+
+    // windraw ether from contract
+    function withdraw() public OnlyOwner {
+        // transfer ether to owner
+        payable(owner).transfer(address(this).balance);
+    }
+}
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,9 +1,91 @@
-const { expect } = require("chai")
+// This file user for test Solodity code.
+// Command: npx hardhat test
+
+// This code for test solidity code in your Mechanic.
+// If you want easy testing go to remix.ethereum.org. It this website no need to write test code.It will make easy testing. No need to understand this code. Just write Solidity code and test blockchain.
+
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
+
+const ID = 1;
+const NAME = "Tv";
+const CATEGORY = "Electronic";
+const IMAGE = "ImgUrl1";
+const PRICE = tokens(1);
+const STOCK = 2;
+const RATING = 4;
 
 describe("Dappazon", () => {
+  let dappazon;
+  let deployer, buyer;
 
-})
+  // Run before every test. Connect to smart contract....
+  beforeEach(async () => {
+    // Hardhat run a test Blockchain in owr system.It has some dummy account in that blockchain.
+    // It have 20 different accounts.
+    [deployer, buyer] = await ethers.getSigners();
+
+    //  Connect smart contract to this file.
+    const Dappazon = await ethers.getContractFactory("Dappazon");
+    // deploy contract to test blockchain
+    dappazon = await Dappazon.deploy();
+  });
+
+  describe("Deployment", async () => {
+    //  owner test...
+    it("Sets the Owner", async () => {
+      const owner = await dappazon.owner();
+      expect(owner).to.equal(deployer.address);
+    });
+  });
+
+  describe("Listing Item", async () => {
+    let transaction;
+
+    beforeEach(async () => {
+      transaction = await dappazon
+        .connect(deployer)
+        .list(ID, NAME, CATEGORY, IMAGE, PRICE, STOCK, RATING);
+
+      await transaction.wait();
+    });
+
+    it("List", async () => {
+      const item = await dappazon.items(ID);
+      expect(item.id).to.equal(ID);
+    });
+  });
+
+  describe("Buying", async () => {
+    let transaction;
+
+    beforeEach(async () => {
+      transaction = await dappazon
+        .connect(deployer)
+        .list(ID, NAME, CATEGORY, IMAGE, PRICE, STOCK, RATING);
+
+      await transaction.wait();
+
+      // Buy item
+      transaction = await dappazon.connect(buyer).buy(ID, { value: PRICE });
+    });
+
+    it("Update contract Balance", async () => {
+      const result = await ethers.provider.getBalance(dappazon.address);
+      expect(result).to.equal(PRICE);
+    });
+
+    it("Buyer's order count", async () => {
+      const result = await dappazon.orderCount(buyer.address);
+      expect(result).to.equal(1);
+    });
+
+    it("Emit buy event", async () => {
+      expect(transaction).to.emit(dappazon, "BuyItem");
+    });
+  });
+});
